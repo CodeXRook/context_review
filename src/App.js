4b6df3f8a7142d0980d2786148dfe9ef23969137
CodeXRook <lacekitfly@gmail.com>
2019-03-23 11:32:49 -0400

    updated func

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import List from './containers/list';
 class App extends Component {
   render() {
     return (
-
      <List />
 
     );
--- src/containers/list.js
+++ src/containers/list.js
@@ -4,7 +4,7 @@ import ListItem from '../components/listitem';
 export default class List extends React.Component {
 
     state ={
-        date: [1, 2, 3, 4, 5]
+        date: [1, 2, 3, 4, 5, 6, 7]
     }
 
     render() {
