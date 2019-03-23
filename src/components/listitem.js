d196410b423969606b7638bea6351b56d44d89a4
CodeXRook <lacekitfly@gmail.com>
2019-03-23 12:59:22 -0400

    added value

--- src/components/listitem.js
+++ src/components/listitem.js
@@ -5,7 +5,11 @@ import ThemeContext from '../contexts/theme';
 export default (props) => {
     return 
     <ThemeContext.Consume>
-        
+        {
+            (value) => {
+                
+            }
+        }
     </ThemeContext.Consume>
     return <li style={{backgroundColor: props.theme.backgroundColor, color: props.theme.color }}>{props.data}</li>
 }
