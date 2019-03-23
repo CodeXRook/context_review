83bb8f69c4bbbb46c74daf05ccef6d0c5c1d75b8
CodeXRook <lacekitfly@gmail.com>
2019-03-23 12:30:33 -0400

    add new static update

--- src/containers/list.js
+++ src/containers/list.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ListItem from '../components/listitem';
+import ThemeContext from '../contexts/theme';
 
 
 export default class List extends React.Component {
+
+    static contextType = ThemeContext;
 
     state = {
         date: [1, 2, 3, 4, 5, 6, 7]
     }
 
     render() {
+        console.log(this.context);
         return (
             <ul>
                 {
